70a31e98368a67a63c79bd66c5ca80d3113b102c
PabloRR100 <pabloruizruiz10@gmail.com>
2025-01-18 10:18:23 +0100

    Finished Enums section

--- Unit1/Enums.playground/Contents.swift
+++ Unit1/Enums.playground/Contents.swift
@@ -5,7 +5,28 @@ enum Ingredient: String {
     case fruit = "fruit"
 }
 
+enum RecipeInformation {
+    case allergens(information: String)
+}
+
 
 let ingredient = Ingredient.chiken
+let recipeInformation = RecipeInformation.allergens(information: "peanuts, milk, gluten")
+
 print(ingredient)
 
+// Ingredient
+switch ingredient {
+case .chiken:
+    print("We use \(ingredient.rawValue)")
+case .lettuce:
+    print("We use \(ingredient.rawValue)")
+case .fruit:
+    print("We use \(ingredient.rawValue)")
+}
+
+// Recipe
+switch recipeInformation {
+case .allergens(let information):
+    print("The meal includes the following allergens: \(information).")
+}
